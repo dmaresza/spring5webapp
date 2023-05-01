52130796bf0960173c18ea1d441380d4dab63221
dmaresza <daniel.a.mares.z@gmail.com>
2023-05-01 14:22:25 -0400

    Added AuthorController class to display list of authors

---
+++ src/main/java/guru/springFramework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guru.springFramework.spring5webapp.controllers;
+
+import guru.springFramework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author danie
+ */
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+}
